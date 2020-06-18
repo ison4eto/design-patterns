b41126fd957b28bdd90e7c3713ab648b6639f2d8
iskra <ison4eto635@gmail.com>
2020-06-18 22:18:33 +0300

    Add AdapterPattern

---
+++ StructuralPatterns/AdapterPattern/AdapterPattern/Compound.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    class Compound
+    {
+        protected string _chemical;
+        protected float _boilingPoint;
+        protected float _meltingPoint;
+        protected string _molecularFormula;
+
+        // Constructor
+        public Compound(string chemical)
+        {
+            this._chemical = chemical;
+        }
+        public virtual void Display()
+        {
+            Console.WriteLine("\nCompound: {0} ------ ", _chemical);
+        }
+    }
+}
---
+++ StructuralPatterns/AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterPattern
+{
+    class Example
+    {
+        static void Main()
+        {
+
+            // Non-adapted chemical compound
+            Compound unknown = new Compound("Unknown");
+            unknown.Display();
+
+            // Adapted chemical compounds
+            Compound water = new RichCompound("Water");
+            water.Display();
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+            Console.ReadKey();
+        }
+    }
+}
